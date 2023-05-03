2a6054c2ef5d95a6b803b499339037bcb366afcb
mcsymiv <s.mcsymiv@gmail.com>
2023-05-03 20:42:29 +0300

    add [url step, test receiver]

--- test/session_test.go
+++ test/session_test.go
@@ -8,34 +8,49 @@ import (
 	"github.com/mcsymiv/go-stripe/session"
 )
 
-var sc *config.SessionConfig
+var st SessionTest
 
-// TestStartFirefox
-// Open new firefox browser
-func TestCreateSession(t *testing.T) {
+type SessionTest struct {
+	Id string
+}
+
+func TestSession(t *testing.T) {
 
 	// Starts firefox browser
 	// Sets new session id to SessionRepo
-	sc = session.NewSession()
-
-	if sc.Id == "" {
-		t.Error("No session id")
+	sc := session.NewSession()
+	st := &SessionTest{
+		Id: sc.Id,
 	}
+
+	// test steps
+	st.GetSessionStatus(t)
+	st.OpenUrl(t)
+	st.CloseSession(t)
 }
 
 // TestGetStatus
 // Prints info on remote to stdout
-func TestGetStatus(t *testing.T) {
+func (st *SessionTest) GetSessionStatus(t *testing.T) {
 	rr, _ := session.DoRequest("get", fmt.Sprintf("%s%s", config.DriverUrl, "/status"), nil)
 
-	fmt.Println(string(rr))
-
+	if string(rr) == "" {
+		t.Errorf("Session status error")
+	}
 }
 
-// TestCloseSession
-// Closes remote session
-func TestCloseSession(t *testing.T) {
-	rr, _ := session.DoRequest("delete", fmt.Sprintf("%s%s/%s", config.DriverUrl, "/session", sc.Id), nil)
+// OpenUrl
+// Goes to url
+func (st *SessionTest) OpenUrl(t *testing.T) {
+	url := []byte(`{"url": "https://google.com"}`)
+
+	_, _ = session.DoRequest("post", fmt.Sprintf("%s/session/%s/url", config.DriverUrl, st.Id), url)
+	rr, _ := session.DoRequest("get", fmt.Sprintf("%s/session/%s/url", config.DriverUrl, st.Id), url)
 
 	fmt.Println(string(rr))
+}
+
+// CloseSession
+func (st *SessionTest) CloseSession(t *testing.T) {
+	session.DoRequest("delete", fmt.Sprintf("%s%s/%s", config.DriverUrl, "/session", st.Id), nil)
 }
