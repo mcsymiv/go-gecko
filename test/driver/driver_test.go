9e47917f4ced4f69cc5871167d51dcbc9c3ce3df
mcsymiv <s.mcsymiv@gmail.com>
2023-09-01 10:27:11 +0300

    add headless arg test

--- test/driver/driver_test.go
+++ test/driver/driver_test.go
@@ -39,3 +39,23 @@ func TestNewDriver(t *testing.T) {
   defer cmd.Process.Kill()
 	defer d.Quit()
 }
+
+// TestNewHeadlessDriver
+// Tests gecko driver and firefox instance
+func TestNewHeadlessDriver(t *testing.T) {
+  // moz:options
+  // Uses headless arg
+	moz := &capabilities.MozOptions{
+		Args: []string{"-headless"},
+	}
+
+  // Connect to the WebDriver instance running locally
+	d, cmd := session.NewDriver(
+    capabilities.ImplicitWait(3000),
+    capabilities.Firefox(moz),
+  )
+
+  // LIFO defer stack to quit firefox, and then kill driver proc
+  defer cmd.Process.Kill()
+	defer d.Quit()
+}
