d2ee671e3aa38342fb3acd8fbf57b0ff73fe4af3
mcsymiv <s.mcsymiv@gmail.com>
2023-11-14 14:52:26 +0200

    rm strategy code

--- capabilities/capabilities.go
+++ capabilities/capabilities.go
@@ -1,6 +1,33 @@
 package capabilities
 
-// Usage:
+type NewSessionCapabilities struct {
+	Capabilities BrowserCapabilities `json:"capabilities"`
+}
+
+type BrowserCapabilities struct {
+	AlwaysMatch AlwaysMatch `json:"alwaysMatch"`
+}
+
+type AlwaysMatch struct {
+	AcceptInsecureCerts bool       `json:"acceptInsecureCerts"`
+	BrowserName         string     `json:"browserName"`
+	Timeouts            Timeouts   `json:"timeouts,omitempty"`
+	MozOptions          MozOptions `json:"moz:firefoxOptions,omitempty"`
+}
+
+type Timeouts struct {
+	Implicit float32 `json:"implicit,omitempty"`
+	PageLoad float32 `json:"pageLoad,omitempty"`
+	Script   float32 `json:"script,omitempty"`
+}
+
+type MozOptions struct {
+	Profile string   `json:"profile,omitempty"`
+	Binary  string   `json:"binary,omitempty"`
+	Args    []string `json:"args,omitempty"`
+}
+
+// CapabilitiesFunc Usage:
 //
 // For the capabilities set with argument:
 //
--- selenium/selenium.go
+++ selenium/selenium.go
@@ -5,13 +5,13 @@ import (
 )
 
 const (
-	// legacyWebElementIdentifier is the string constant used in the old Selenium 2 protocol
+	// LegacyWebElementIdentifier is the string constant used in the old Selenium 2 protocol
 	// WebDriver JSON protocol that is the key for the map that contains an
 	// unique element identifier.
 	// This value is ignored in element id retreival
 	LegacyWebElementIdentifier = "ELEMENT"
 
-	// webElementIdentifier is the string constant defined by the W3C Selenium 3 protocol
+	// WebElementIdentifier is the string constant defined by the W3C Selenium 3 protocol
 	// specification that is the key for the map that contains a unique element identifier.
 	WebElementIdentifier = "element-6066-11e4-a52e-4f735466cecf"
 
