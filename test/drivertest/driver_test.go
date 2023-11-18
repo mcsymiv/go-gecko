d6824388d57e4e87a1b613407f35bef79b09a3a4
mcsymiv <s.mcsymiv@gmail.com>
2023-11-18 17:04:13 +0200

    add api call test

--- test/drivertest/driver_test.go
+++ test/drivertest/driver_test.go
@@ -1,7 +1,9 @@
 package drivertest
 
 import (
+	"github.com/mcsymiv/go-gecko/driver"
 	"github.com/mcsymiv/go-gecko/hooks"
+	"log"
 	"testing"
 	"time"
 
@@ -37,6 +39,33 @@ func TestDriver(t *testing.T) {
 		// capabilities.Port(":9515"),
 	)
 	defer tear()
+
+	d.Open("https://google.com")
+	time.Sleep(2 * time.Second)
+}
+
+// TestDriver
+// Opens firefox as default driver
+// With default capabilities for driver with host, url: http://localhost:4444
+func TestDriverWithExternalApiCall(t *testing.T) {
+
+	// Connect to the WebDriver instance running locally
+	d, tear := hooks.Driver(
+		capabilities.ImplicitWait(3000),
+		// chrome caps commented out by design
+		// capabilities.BrowserName("chrome"),
+		// capabilities.Port(":9515"),
+	)
+	defer tear()
+
+	// Updates driver client url to call external API
+	res, _ := d.MakeRequest(
+		func(ro *driver.RequestOptions) {
+			ro.Url = "https://pokeapi.co/api/v2/pokemon/ditto"
+		},
+	)
+
+	log.Print(string(res))
 
 	d.Open("https://google.com")
 	time.Sleep(2 * time.Second)
